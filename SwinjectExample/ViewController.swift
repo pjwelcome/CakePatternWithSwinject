53480b94c6594ebb7581b255f8451490ae03d88d
Peter-John Welcome <Peter-John.Welcome@Standardbank.co.za>
2018-04-14 20:08:56 +0200

    Initial Commit

---
+++ SwinjectExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwinjectExample
+//
+//  Created by pjapple on 2018/04/14.
+//  Copyright © 2018 Multimeleon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
