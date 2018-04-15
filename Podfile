c7563ef1e0152870bab573d2a45a05ef14861bc5
Peter-John Welcome <Peter-John.Welcome@Standardbank.co.za>
2018-04-15 14:22:32 +0200

    Create a pod file Added the Swinject pod created a dependency container that holds the swinject container Added registrable and Resolvable protocols for the container Created a dependency Resolve that will resolve implementations based on the dependency

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwinjectExample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwinjectExample
+	pod 'Swinject'
+  target 'SwinjectExampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- SwinjectExample/ViewController.swift
+++ SwinjectExample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Swinject
 
 class ViewController: UIViewController {
 
@@ -23,3 +24,57 @@ class ViewController: UIViewController {
 
 }
 
+public protocol Resolvable {
+    func resolve<T>(_ dependency: T.Type) -> T
+    func reset()
+}
+
+public protocol Registrable {
+    func register<T>(depedency: T.Type, implemenation: @escaping () -> T, objectScope: ObjectScope)
+}
+
+public protocol Resolving {
+    static func resolve<T>(_ dependency: T.Type) -> T
+    static func reset()
+}
+
+class Resolver {
+    private static var container: Resolvable = DepedencyContainer.instance
+}
+
+extension Resolver : Resolving {
+    
+    public static func resolve<T>(_ dependency: T.Type) -> T {
+        return container.resolve(dependency)
+    }
+    public static func reset() {
+        container.reset()
+    }
+}
+
+class DepedencyContainer {
+    
+    private let container = Container()
+    public static let instance = DepedencyContainer()
+}
+
+extension DepedencyContainer : Registrable {
+    
+    public func register<T>(depedency: T.Type, implemenation: @escaping () -> T, objectScope: ObjectScope = .graph) {
+        container.register(depedency, factory: { _ in implemenation() }).inObjectScope(objectScope)
+    }
+}
+
+extension DepedencyContainer : Resolvable {
+    
+    public func resolve<T>(_ dependency: T.Type) -> T {
+        guard let implementation = container.resolve(dependency) else {
+            fatalError("Nothing to Resolve")
+        }
+        return implementation
+    }
+    
+    public func reset() {
+        container.removeAll()
+    }
+}
